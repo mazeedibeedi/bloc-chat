9b0576f309c22a0264e0a030174de845efb48abf
Ahmed Almazidi <a.almazidi92@gmail.com>
2017-03-21 14:27:24 +0300

    Created Message service and update the home view with a basic view

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,7 +1,10 @@
 (function() {
-    function HomeCtrl(Room, $uibModal){
+    function HomeCtrl(Room, Message, $uibModal){
         this.roomData = Room.all;
-        
+        this.activeRoom = null;
+        this.messages = function(roomId) {
+            return Message.getByRoomId(roomId);
+        }
         this.open = function() {
             $uibModal.open({
                 templateUrl: '/templates/modal.html',
@@ -9,9 +12,12 @@
                 controllerAs: 'modal',
             });
         };
+        this.select = function(room) {
+            this.activeRoom = room;
+        }
     }
     
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 })();
